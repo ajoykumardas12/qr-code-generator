d5b89b5afdcc336b8505c30c73ee490619539341
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-03-02 09:10:00 +0530

    active-nav

--- src/components/NavArea.jsx
+++ src/components/NavArea.jsx
@@ -1,27 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./NavArea.css";
 
 function NavArea() {
+    const path = useLocation();
+
     return (
         <nav className="nav-area">
-            <Link className="nav-item text" to="/">
+            <Link className={`nav-item text ${path.pathname === "/" ? "active" : ""}`} to="/">
                 <ion-icon name="document-text-outline"></ion-icon>
                 <p className="nav-item-name">Text</p>
             </Link>
-            <Link className="nav-item website" to="/website">
+            <Link className={`nav-item website ${path.pathname === "/website" ? "active" : ""}`} to="/website">
                 <ion-icon name="globe-outline"></ion-icon>
                 <p className="nav-item-name">Website</p>
             </Link>
-            <Link className="nav-item email" to="/email">
+            <Link className={`nav-item email ${path.pathname === "/email" ? "active" : ""}`} to="/email">
                 <ion-icon name="mail-outline"></ion-icon>
                 <p className="nav-item-name">Email</p>
             </Link>
-            <Link className="nav-item sms" to="/sms">
+            <Link className={`nav-item sms ${path.pathname === "/sms" ? "active" : ""}`} to="/sms">
                 <ion-icon name="send-outline"></ion-icon>
                 <p className="nav-item-name">SMS</p>
             </Link>
-            <Link className="nav-item phone" to="/phone">
+            <Link className={`nav-item phone ${path.pathname === "/phone" ? "active" : ""}`} to="/phone">
                 <ion-icon name="call-outline"></ion-icon>
                 <p className="nav-item-name">Phone</p>
             </Link>
