62e82914dcf3bb46472d540cd965c090e575822b
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-09-02 23:54:46 +0530

    header design update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,7 @@ import "./Header.css";
 function Header() {
   return (
     <header>
-      <h1>
-        <a href="/">QR Code Generator</a>
-      </h1>
+      <h1>QR Code Generator</h1>
     </header>
   );
 }
